736ba24b4885ab0ae66322870c9a20d888e91361
DDnsk <380712421@qq.com>
2017-02-21 11:02:02 +0800

    Update tfidf.py
    main 函数封装

--- tfidf.py
+++ tfidf.py
@@ -22,34 +22,39 @@ def idf(word, count_list): #计算idf
     return math.log10(len(count_list) / (n_containing(word, count_list)))
 def tfidf(word, count, count_list): #计算tf*idf
     return tf(word, count) * idf(word, count_list)
-f=open('C:\Users\swy\Desktop\miss.txt')
-fr=f.read()
-fr=fr.replace('\"','')
-fr=fr.replace('-','')
-fr=fr.replace('Mr. ','Mr,') #Mr.的“.”很容易与巨好混淆，先去掉，随后再补充上
-fr=fr.replace('Mrs. ','Mrs,')
-fr=fr.replace('. ','.')
-fr=fr.replace('.\n','.')
-for i in('!','?','\n'): #将换行的符号全部替换成“.”用来分句
-    fr=fr.replace(i,'.')
-length = fr.count(".")
-print length
-u=range(1,1000)
-count=range(1,1000)
-final=range(1,1000)
-u[1:length]=fr.split('.',length-1)
-for i in range(1,length):
-    u[i]=u[i].replace('Mr,','Mr.') #去掉逗号等分句时不会去掉的符号
-    u[i]=u[i].replace('Mrs,','Mrs.')
-    for j in (',','(',')',';'):
-        u[i]=u[i].replace(j,'') #去停用词
-    tokens=get_tokens(u[i])
-    filtered = [w for w in tokens if not w in stopwords.words('english')]
-    stemmer = PorterStemmer()
-    stemmed = stem_tokens(filtered, stemmer)
-    count[i]=Counter(stemmed)
-    print count[i]
-countlist=[count[j] for j in range (1,length)]
-for i in range(1,length):
-    final[i] = {word: tfidf(word, count[i], countlist)/math.sqrt(sum(tfidf(word,count[i],countlist)*tfidf(word,count[i],countlist) for word in count[i])) for word in count[i]}
-    print final[i]
+
+# 主体函数
+def main():
+    f=open('C:\Users\swy\Desktop\miss.txt')
+    fr=f.read()
+    fr=fr.replace('\"','')
+    fr=fr.replace('-','')
+    fr=fr.replace('Mr. ','Mr,') #Mr.的“.”很容易与巨好混淆，先去掉，随后再补充上
+    fr=fr.replace('Mrs. ','Mrs,')
+    fr=fr.replace('. ','.')
+    fr=fr.replace('.\n','.')
+    for i in('!','?','\n'): #将换行的符号全部替换成“.”用来分句
+        fr=fr.replace(i,'.')
+    length = fr.count(".")
+    print length
+    u=range(1,1000)
+    count=range(1,1000)
+    final=range(1,1000)
+    u[1:length]=fr.split('.',length-1)
+    for i in range(1,length):
+        u[i]=u[i].replace('Mr,','Mr.') #去掉逗号等分句时不会去掉的符号
+        u[i]=u[i].replace('Mrs,','Mrs.')
+        for j in (',','(',')',';'):
+            u[i]=u[i].replace(j,'') #去停用词
+        tokens=get_tokens(u[i])
+        filtered = [w for w in tokens if not w in stopwords.words('english')]
+        stemmer = PorterStemmer()
+        stemmed = stem_tokens(filtered, stemmer)
+        count[i]=Counter(stemmed)
+        print count[i]
+    countlist=[count[j] for j in range (1,length)]
+    for i in range(1,length):
+        final[i] = {word: tfidf(word, count[i], countlist)/math.sqrt(sum(tfidf(word,count[i],countlist)*tfidf(word,count[i],countlist) for word in count[i])) for word in count[i]}
+        print final[i]
+if __name__ == '__main__':
+    main()
